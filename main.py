0ce02846c9f343f9fd6b7cb0929ec4e73abb8b53
Saimun Hassan <saimun4u@gmail.com>
2023-04-30 16:40:00 -0400

    Developed todolist and logic for user input

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 todolist = []
 print('Enter a todo: ')
 
+user_todo = input()
+todolist.append(user_todo.capitalize())
+
 while True:
-    user_todo = input()
-    todolist.append(user_todo.capitalize())
+    print('Do you want to add another todo? yes or no' )
+    response = 'yes' or 'no'
+    if response == 'yes':
+        user_todo = input('Enter a todo: ')
+        todolist.append(user_todo.capitalize())
     break
+
+print(f'Your todos are: {todolist}')
